9e29def5c76248c3a0e787f5d58beda22a5e4e3f
Ernesto-GT <ernesto.guerratrvn@uanl.edu.mx>
2023-09-19 22:27:22 -0600

    Practica_3 - Completada

--- Practica_3.py
+++ Practica_3.py
@@ -1,27 +1,46 @@
 import csv
 import pandas as pd
+#ED->Estadistica Descriptiva
+#Analisis de la ED al relacionar cada uno de los Generos con la Puntuacion otorgada a cada serie
+def EstadisticaPorGeneroYPuntajes():
+    resultado = df.groupby('Genres')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
+    #print(resultado)
+    resultado.to_csv('DataSet_Genero-Puntajes.csv')
 
-#Cantidad de series registradas agrupadas por genero
-def Cantidad_Genero():
-    resultado = df.groupby('Genres')['Name'].count()
-    print(resultado)
-#Promedio de puntaje de todas las series agrupadas por por genero 
-def Puntaje_Promedio_Genero():
-    resultado = df.groupby('Genres')['Score'].mean()
-    print(resultado)
-#Cantidad de seguidores registrados para cada tipo de anime
-def Cantidad_Miembros_Tipo():
-    resultado = df.groupby('Type')['Members'].sum()
-    print(resultado)
-#
+#Analisis de la ED al relacionar cada uno de los Generos con la cantidad de Seguidores de la serie 
+def EstadisticaPorGeneroYSeguidores():
+    resultado = df.groupby('Genres')['Members'].agg(['sum', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Seguidores', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
+    #print(resultado)
+    resultado.to_csv('DataSet_Genero-Seguidores.csv')
 
-def Min_Max_Puntaje():
-    resultado = df['Score'].min()
-    resultado2 = df.groupby('Name')['Score'].max()
-    print(resultado)
+#Analisis de la ED al relacionar cada uno de los distintos Medios Audio-Visuales con la Puntuacion otorgada a la serie 
+def EstadisticaPorTipoYPuntajes():
+    resultado = df.groupby('Type')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
+    #print(resultado)
+    resultado.to_csv('DataSet_Tipo-Puntajes.csv')
 
+#Analisis de la ED respecto a los puntajes otorgados a las series, en base al año en que salieron
+def EstadisticaPorFechaYPuntajes():
+    resultado = df.groupby('Aired')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
+    #print(resultado)
+    resultado.to_csv('DataSet_Fecha-Puntajes.csv')
+
+#Analisis de la ED al relacionar cada uno de los Generos con la cantidad de Episodios que tiene cada titulo 
+def EstadisticaPorGeneroYEpisodios():
+    resultado = df.groupby('Genres')['Episodes'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
+    #print(resultado)
+    resultado.to_csv('DataSet_Genero-Episodios.csv')
+
+#Lectura de archivo csv
 df = pd.read_csv('DataSetClean.csv')
-#Cantidad_Genero()
-#Puntaje_Promedio_Genero()
-#Cantidad_Miembros_Tipo()
-Min_Max_Puntaje()
+#Ejecucios de las pruebas de Estadistica Descriptiva
+EstadisticaPorGeneroYPuntajes()
+EstadisticaPorGeneroYSeguidores()
+EstadisticaPorTipoYPuntajes()
+EstadisticaPorFechaYPuntajes()
+EstadisticaPorGeneroYEpisodios()
