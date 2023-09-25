2ce0ba27001ded26dccd4cef3e5c2ee8aee5b0b1
Ernesto-GT <ernesto.guerratrvn@uanl.edu.mx>
2023-09-24 23:14:43 -0600

    Practica 4 - Comienzo

--- Practica_3.py
+++ Practica_3.py
@@ -6,35 +6,35 @@ def EstadisticaPorGeneroYPuntajes():
     resultado = df.groupby('Genres')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
     resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
     #print(resultado)
-    resultado.to_csv('DataSet_Genero-Puntajes.csv')
+    resultado.to_csv('Tablas_P3/DataSet_Genero-Puntajes.csv')
 
 #Analisis de la ED al relacionar cada uno de los Generos con la cantidad de Seguidores de la serie 
 def EstadisticaPorGeneroYSeguidores():
     resultado = df.groupby('Genres')['Members'].agg(['sum', 'mean', 'min', 'max', 'std', 'var'])
     resultado.columns = ['Total_Seguidores', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
     #print(resultado)
-    resultado.to_csv('DataSet_Genero-Seguidores.csv')
+    resultado.to_csv('Tablas_P3/DataSet_Genero-Seguidores.csv')
 
 #Analisis de la ED al relacionar cada uno de los distintos Medios Audio-Visuales con la Puntuacion otorgada a la serie 
 def EstadisticaPorTipoYPuntajes():
     resultado = df.groupby('Type')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
     resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
     #print(resultado)
-    resultado.to_csv('DataSet_Tipo-Puntajes.csv')
+    resultado.to_csv('Tablas_P3/DataSet_Tipo-Puntajes.csv')
 
 #Analisis de la ED respecto a los puntajes otorgados a las series, en base al año en que salieron
 def EstadisticaPorFechaYPuntajes():
     resultado = df.groupby('Aired')['Score'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
     resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
     #print(resultado)
-    resultado.to_csv('DataSet_Fecha-Puntajes.csv')
+    resultado.to_csv('Tablas_P3/DataSet_Fecha-Puntajes.csv')
 
 #Analisis de la ED al relacionar cada uno de los Generos con la cantidad de Episodios que tiene cada titulo 
 def EstadisticaPorGeneroYEpisodios():
     resultado = df.groupby('Genres')['Episodes'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
     resultado.columns = ['Total_Animes', 'Promedio', 'Puntaje_Minimo', 'Puntaje_Maximo', 'Desviacion_Estandar', 'Varianza']
     #print(resultado)
-    resultado.to_csv('DataSet_Genero-Episodios.csv')
+    resultado.to_csv('Tablas_P3/DataSet_Genero-Episodios.csv')
 
 #Lectura de archivo csv
 df = pd.read_csv('DataSetClean.csv')
---
+++ Practica_4.py
@@ -0,0 +1,27 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('DataSetClean.csv')
+
+#Grafica de Bigote, tomando los Puntajes otorgados a cada serie, agrupando por Genero
+sns.boxplot(x="Score", y="Genres", data=df)
+plt.savefig(f"Graficas_P4/GBigote_Puntajes-Genero.png")
+#plt.show()
+plt.gcf().clear()
+
+#Grafica de Bigote, tomando los Puntajes otorgados a cada serie, agrupando por Tipo de Visualizacion 
+sns.boxplot(x="Score", y="Source", data=df)
+plt.savefig(f"Graficas_P4/GBigote_Puntajes-Tipos.png")
+#plt.show()
+plt.gcf().clear()
+
+#Grafica de Pastel, tomando en cuenta la catidad de Seguidores que tiene cada Tipo de Visualizacion
+df_v = df.groupby('Type')['Members'].sum()
+plot = df_v.plot.pie(y='Type', figsize=(10,10), autopct="%1.0f%%")
+resultado = df.groupby('Type')['Members'].sum()
+print(resultado)
+plt.savefig(f"Graficas_P4/GPastel_Seguidores-Tipos.png")
+#plt.show()
+plt.gcf().clear()
