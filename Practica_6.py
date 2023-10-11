e3778707061bbbaf67f1592712d41e54a090b321
Ernesto-GT <ernesto.guerratrvn@uanl.edu.mx>
2023-10-10 23:11:27 -0600

    Practica 6

--- Practica_6.py
+++ Practica_6.py
@@ -1,17 +1,49 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 datos = pd.read_csv('DataSetClean.csv')
-datos = datos.where(datos["Type"] == 'Movie')   
+
+def RL_MoviesAdventure():
+    datos2 = datos.where((datos["Type"] == 'Movie') & (datos["Genres"] == 'Adventure'))  
+
+    # Gráfico de dispersión con la línea de regresión
+    sns.lmplot(x='Aired', y='Score', data=datos2, line_kws={'color': 'red'})
+    prom = datos2['Score'].mean()
+    # Promedio
+    plt.axhline(y=prom, xmin=0.01, xmax=0.99, color="green")
+
+    # Muestra el gráfico
+    plt.xlabel('Puntaje')
+    plt.ylabel('Fecha Emision')
+    plt.title('Regresión Lineal: Puntaje vs Fecha Emision \n (Peliculas de Aventura)')
+    plt.show()
+    #plt.savefig("RegresionLineal_P6/Movies-Adventure_Fecha-Puntaje.png")
+    plt.close()
+
+def RL_MangaSports():
+    datos2 = datos.where((datos["Source"] == 'Manga') & (datos["Genres"] == 'Sports'))  
+
+    # Gráfico de dispersión con la línea de regresión
+    sns.lmplot(x='Aired', y='Score', data=datos2, line_kws={'color': 'red'})
+    prom = datos2['Score'].mean()
+    # Promedio
+    plt.axhline(y=prom, xmin=0.01, xmax=0.99, color="green")
+
+    # Muestra el gráfico
+    plt.xlabel('Miembros')
+    plt.ylabel('Fecha Emision')
+    plt.title('Regresión Lineal: Puntajes vs Fecha Emision \n (Series de Deportes basadas en Manga)')
+    plt.show()
+    plt.savefig("RegresionLineal_P6/Manga-Deportes_Fecha-Puntaje.png")
+    plt.close()
+
+#RL_MoviesAdventure()
+RL_MangaSports()
 
 
-# Usa seaborn para crear un gráfico de dispersión con la línea de regresión
-sns.lmplot(x='Aired', y='Score', data=datos)
-# Muestra el gráfico
-plt.xlabel('Puntaje')
-plt.ylabel('Fecha Emision')
-plt.title('Regresión Lineal: Puntaje vs Fecha Emision')
-plt.show()
-#plt.savefig('RegresionLineal.png')
-plt.close()
+
+
+
+
